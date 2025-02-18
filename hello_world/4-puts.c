f945689a9b912fcdc613241b53c65243a26f261a
Un4givenlight <fernandocolon2699@gmail.com>
2025-02-18 14:45:04 -0400

    put

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that prints a line with puts functio
  * Return: 0 (Success)
  */
-int main (void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
