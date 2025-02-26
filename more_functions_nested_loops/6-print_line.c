dd3179116061180a42d8eaa76d99ef4cbb3897c4
Un4givenlight <fernandocolon2699@gmail.com>
2025-02-26 10:16:52 -0400

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - draw a straight line in terminal
+ * @n: input interger
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
