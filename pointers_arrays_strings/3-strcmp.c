88c80e04f9ff5303d61eb885431f8e3bd6a99765
Un4givenlight <fernandocolon2699@gmail.com>
2025-03-02 17:25:50 -0400

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s1: first string
  * @s2: second string
  *
- * Return; (0) when its same
+ * Return: (0) when its same
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' && b == 0; i++)
 	{
-		b = s1 [i] - s2[i];
+		b = s1[i] - s2[i];
 	}
 
 	return (b);
