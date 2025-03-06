3f00f17833da8b843b7cb02208d96a5a468a8bfa
Un4givenlight <fernandocolon2699@gmail.com>
2025-03-06 15:12:39 -0400

    prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,36 +1,29 @@
 #include "main.h"
 #include <stdio.h>
-
- int check_prime(int n, int i);
-
- /**
-  * is_prime_number - Returns if a number is prime 
-  * @n: the number to be checked
-  *
-  * Return: integer value
-  */
- int is_prime_number(int n)
-{
-	return (check_prime(n, 1));
-}
-
 /**
- * check_prime - Check if number is prime 
- * @n: the number to be checked
- * @i: the iteration times
- *
- * Return: 1 for prime or 0 composite
+ * prime_a - Calculate if its a prime
+ * @a: input
+ * @b: divisor
+ * Return: (0)
  */
-int check_prime(int n, int i)
+int prime_a(int a, int b)
 {
-	if (n <= 1)
+	if (a <= 1 || (a != b && a % b == 0))
+	{
 		return (0);
-
-	if (n % i == 0 && i > 1)
-		return (0);
-
-	if ((n / i) < i)
+	}
+	else if (a == b)
+	{
 		return (1);
-
-	return (check_prime(n, i + 1));
+	}
+	return (prime_a(a, b + 1));
+}
+/**
+ * is_prime_number - detect if its a prime no
+ * @n: the input number
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
 }
--- recursion/main.h
+++ recursion/main.h
@@ -8,4 +8,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
 int is_prime_number(int n);
-int check_prime(int n, int i);
+int prime_a(int a, int b);
